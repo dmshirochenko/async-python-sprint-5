8b7164db917672d4748297c09376a7a3a01b2f0b
Dmitriy Shirochenko <d.shirochenko@criteo.com>
2024-02-20 00:27:35 +0100

    added real auth for file_storage service

--- file_storage_api/src/api/v1/file_storage_routes.py
+++ file_storage_api/src/api/v1/file_storage_routes.py
@@ -1,7 +1,8 @@
 import uuid
 
+import httpx
 from fastapi import File as FastAPIFile
-from fastapi import APIRouter, UploadFile, Depends, HTTPException
+from fastapi import APIRouter, UploadFile, Depends, HTTPException, status, Request
 from fastapi.responses import RedirectResponse
 from sqlalchemy import select
 from sqlalchemy.future import select
@@ -16,8 +17,22 @@ from src.services.utils import ensure_bucket_exists, upload_file_to_minio, save_
 router = APIRouter()
 
 
-async def get_current_user():
-    return {"user_id": 1}
+async def get_token_header(request: Request):
+    if "Authorization" not in request.headers:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authorization header missing")
+    return request.headers["Authorization"].split(" ")[1]
+
+
+async def get_current_user(token: str = Depends(get_token_header)):
+    auth_url = f"{settings.auth_service_url}/auth/v1/user/by-token"
+    async with httpx.AsyncClient() as client:
+        response = await client.get(auth_url, headers={"Authorization": f"Bearer {token}"})
+        if response.status_code != 200:
+            raise HTTPException(status_code=response.status_code, detail="Invalid token or auth server error")
+        user_data = response.json()
+        if not user_data["is_active"]:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="User is inactive")
+        return {"user_id": user_data["id"], "username": user_data["username"]}
 
 
 @router.get("/file-storage/v1/ping")
@@ -25,7 +40,7 @@ async def ping_service():
     return {"message": "Service is up and running"}
 
 
-@router.get("/file-storage/files/list", response_model=FileListResponse)
+@router.get("/file-storage/v1/files/list", response_model=FileListResponse)
 async def list_files(user: dict = Depends(get_current_user), db: AsyncSession = Depends(get_async_session)):
     async with db as session:
         result = await session.execute(select(File).where(File.user_id == user["user_id"]))
@@ -34,7 +49,7 @@ async def list_files(user: dict = Depends(get_current_user), db: AsyncSession = 
         return FileListResponse(user_id=user["user_id"], files=files_info)
 
 
-@router.post("/file-storage/files/upload", response_model=FileInfoResponse)
+@router.post("/file-storage/v1/files/upload", response_model=FileInfoResponse)
 async def upload_file(
     upload_file: UploadFile = FastAPIFile(...),
     user: dict = Depends(get_current_user),
@@ -52,7 +67,7 @@ async def upload_file(
     return FileInfoResponse.from_orm(file_info)
 
 
-@router.get("/file-storage/files/download")
+@router.get("/file-storage/v1/files/download")
 async def download_file(
     file_id: uuid.UUID, user: dict = Depends(get_current_user), db: AsyncSession = Depends(get_async_session)
 ):
